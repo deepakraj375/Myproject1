c0358903a64d10b8a9fbd93d173948c6c4024fee
deepakraj375 <deepankumar16696@gmail.com>
2024-07-30 18:55:51 +0530

    push

---
+++ src/test/java/com/git/Sample.java
@@ -0,0 +1,21 @@
+package com.git;
+
+import java.util.Iterator;
+
+public class Sample {
+	public static void main(String[] args) {
+		String a="Deepankumar,naven@.ravi";
+		String c="[,.@]";
+		String[] split = a.split(c);
+	for (String string1 : split) {
+		
+	
+		System.out.println(string1);
+		
+	} {
+		
+	}
+		
+	}
+
+}
